f6a9e2355542f929b01c84f2ffe9bfcd36a51106
veeaangupta <95080000+veeaangupta@users.noreply.github.com>
2021-11-26 17:06:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function on(){
+document.getElementById('bulb').src='https://s3-projectsassets.whjr.online/On+Bulb.png' 
+} 
+
+function off(){
+
+document.getElementById('bulb').src='https://s3-projectsassets.whjr.online/Off+Bulb.png' 
+}
